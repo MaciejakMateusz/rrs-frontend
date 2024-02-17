8b0a8292095d0750df20293b618b1686736e2b99
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-02-17 22:00:15 +0100

    Expanded utils.js by adding deleteCookie() function

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,10 @@ const getCookie = cookieName => {
     return cookie[cookieName];
 }
 
+export function deleteCookie(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 const decodeToken = urlEncodedString => {
     let decodedUriComponent = decodeURIComponent(urlEncodedString.replace(/\+/g, ' ').replace(' path', ''));
     return JSON.parse(decodedUriComponent).accessToken;
